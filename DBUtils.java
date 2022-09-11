5532e74436996aa509bea521b9bbad64c0740774
Thauti Chandrasheker <thauti.chandrasheker257@gmail.com>
2022-09-11 07:53:43 +0530

    one go to git local repo DBUtils.java

--- DBUtils.java
+++ DBUtils.java
@@ -1,3 +1,6 @@
 Hello guys its DBUtils file
 
 Add nw line for check the local repo file editing
+
+
+Adding one moreline for check the one command to local repo from working area i.i git commit -a -m "commit message" file name
