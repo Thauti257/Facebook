1c4654c4425627a382f1a64ae93ec2d8bd942035
Thauti Chandrasheker <thauti.chandrasheker257@gmail.com>
2022-09-11 07:56:06 +0530

    a

--- DBUtils.java
+++ DBUtils.java
@@ -4,3 +4,5 @@ Add nw line for check the local repo file editing
 
 
 Adding one moreline for check the one command to local repo from working area i.i git commit -a -m "commit message" file name
+
+Adding one more line for the testing the command -m -a or -a -m
